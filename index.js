f858e9ef47aaa159e82d1a7ec5bfb090634592b6
unknown <basvanderschoot@SKPC0749.Newman.brd>
2016-02-05 11:25:51 +0100

    boodschap

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.use(express.static(__dirname + '/public'));
 
 app.listen(process.env.PORT || 3000);
 
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);

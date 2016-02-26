07750e2d5ab96554664c8eaf40ca5cce5e38a3f4
unknown <basvanderschoot@SKPC0749.Newman.brd>
2016-02-26 11:50:59 +0100

    nog een skwekkerige update voor al jullie swekkers

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
- var express =      require('express');
- var server  =      express();
- var ejs = require('ejs'); 
- ejs.open = '{{'; 
- ejs.close = '}}';
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
- server.use(express.compress());
+ app.use(express.static(__dirname + '/www'));
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
 
- server.configure(function(){
-   server.set("view options", {layout: false});  
-   server.engine('html', require('ejs').renderFile); 
-   server.use(server.router);
-   server.set('view engine', 'html');
-   server.set('views', __dirname + "/www");
- });
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
 
-
- server.all("*", function(req, res, next) {
-     var request = req.params[0];
-
-         if((request.substr(0, 1) === "/")&&(request.substr(request.length - 4) === "html")) {
-         request = request.substr(1);
-         res.render(request);
-     } else {
-         next();
-     }
-
- });
-
- server.use(express.static(__dirname + '/www'));

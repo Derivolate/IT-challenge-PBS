7680709d436e90f31a3cd26e0e94349093013544
Luc den Ridder <lucdenridder@newman.brd>
2016-03-02 09:09:55 +0100

    Fixed some javascript

--- www/app.js
+++ www/app.js
@@ -1,26 +1,21 @@
-var position = 0;
-var main = function() {
-  /* Push the body and the nav over by 285px over */
-  if (position = 0){
-  $('.icon-menu').click(function() {
-    position = 1;
-	$('.left-sidebar').animate({
-      left: "105px"
-    }, 200);
-
-  });
-};
-els{
-  /* Then push them back */
-  $('.icon-menu').click(function() {
-    position = 0;
-	$('.left-sidebar').animate({
-      left: "8px"
-    }, 200);
-
-  });
- };
-};
-
-
-$(document).ready(main);
+$(document).ready(function() {
+	var expanded = false;
+	
+	$('.icon-menu').click(function() 
+	{
+		if (expanded === false)
+		{
+			expanded = true;
+			$('.left-sidebar').animate({
+				left: "105px"
+			}, 200);
+		}
+		else if (expanded === true)
+		{
+			expanded = false;
+			$('.left-sidebar').animate({
+				left: "8px"
+			}, 200);
+		}
+	});
+});

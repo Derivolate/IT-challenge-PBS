775319030d60ff3f9eab5aef95fb479d322552b7
Bastiaan van Hoorn <Bastiaan97.bvh@gmail.com>
2016-03-07 19:49:38 +0100

    Working

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ var db = mysql.createConnection({
     user: 'root',
     password: 'NEWPASSWORD',
     database: 'ITC'
-})
+});
 
-db.connect(function(err){
-	if (err)console.log(err)
-})
+db.connect(function(err) {
+    if (err) console.log(err)
+});
 
 app.use(express.static(__dirname + '/www'));
 app.set('view engine', 'ejs');
@@ -40,25 +40,25 @@ app.get('/results', function(req, res) {
 http.listen(50002, function() {
     console.log('listening on *:50002');
 });
-=======
-io.on('connection', function(socket){
-  socket.on('zoek', function(msg){
-	msg = msg.replace("?", "").replace("'", "''").replace("%", "");
-	var search = "SELECT job FROM jobs WHERE job LIKE '%" + msg + "%' ORDER BY CASE WHEN job LIKE '" + msg + "%' THEN 0 ELSE 1 END, job;";
-	db.query(search, function(err, rows){
-		if (err) throw err;
-		socket.emit('zoek', rows);
-	});
-  });
-  socket.on('beroep', function(msg){
-    var newjobs = msg.split("+");
-    var i = 0;
-    for (i in newjobs){
-    	var q = 'INSERT INTO jobs (job) VALUES ("' + newjobs[i] + '");';
-		db.query(q, function(err, rows){});
-    }
-  });
-  socket.on('pform', function(data){
-    console.log("swek ingestuurd");
-  });
+
+io.on('connection', function(socket) {
+    socket.on('zoek', function(msg) {
+        msg = msg.replace("?", "").replace("'", "''").replace("%", "");
+        var search = "SELECT job FROM jobs WHERE job LIKE '%" + msg + "%' ORDER BY CASE WHEN job LIKE '" + msg + "%' THEN 0 ELSE 1 END, job;";
+        db.query(search, function(err, rows) {
+            if (err) throw err;
+            socket.emit('zoek', rows);
+        });
+    });
+    socket.on('beroep', function(msg) {
+        var newjobs = msg.split("+");
+        var i = 0;
+        for (i in newjobs) {
+            var q = 'INSERT INTO jobs (job) VALUES ("' + newjobs[i] + '");';
+            db.query(q, function(err, rows) {});
+        }
+    });
+    socket.on('pform', function(data) {
+        console.log("swek ingestuurd");
+    });
 });

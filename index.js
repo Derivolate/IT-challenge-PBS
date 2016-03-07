84d87a92a30e9fe0238a6d6b6bd53c6c8d05bc27
bartvanderhoeven <bartvanderhoeven12@gmail.com>
2016-03-07 20:52:10 +0100

    Opnieuw update van suggestielijstje. Begin aan het aanmaken van nieuwe gebruikers in de database

--- index.js
+++ index.js
@@ -14,6 +14,10 @@ db.connect(function(err) {
     if (err) console.log(err)
 });
 
+function getSafeQuery(inputquery){
+	return inputquery.replace("?", "").replace("'", "''").replace("%", "");
+}
+
 app.use(express.static(__dirname + '/www'));
 app.set('view engine', 'ejs');
 
@@ -41,24 +45,46 @@ http.listen(50002, function() {
     console.log('listening on *:50002');
 });
 
-io.on('connection', function(socket) {
-    socket.on('zoek', function(msg) {
-        msg = msg.replace("?", "").replace("'", "''").replace("%", "");
-        var search = "SELECT job FROM jobs WHERE job LIKE '%" + msg + "%' ORDER BY CASE WHEN job LIKE '" + msg + "%' THEN 0 ELSE 1 END, job;";
-        db.query(search, function(err, rows) {
-            if (err) throw err;
-            socket.emit('zoek', rows);
-        });
-    });
-    socket.on('beroep', function(msg) {
-        var newjobs = msg.split("+");
-        var i = 0;
-        for (i in newjobs) {
-            var q = 'INSERT INTO jobs (job) VALUES ("' + newjobs[i] + '");';
-            db.query(q, function(err, rows) {});
-        }
-    });
-    socket.on('pform', function(data) {
-        console.log("swek ingestuurd");
-    });
+io.on('connection', function(socket){
+  socket.on('zoek', function(msg){
+	msg = getSafeQuery(inputquery);
+	var search = "SELECT job FROM jobs WHERE job LIKE '%" + msg + "%' ORDER BY CASE WHEN job LIKE '" + msg + "%' THEN 0 ELSE 1 END, job;";
+	db.query(search, function(err, rows){
+		if (err) throw err;
+		socket.emit('zoek', rows);
+	});
+  });
+  socket.on('beroep', function(msg){
+	msg = getSafeQuery(msg);
+    var newjobs = msg.split("+");
+    var i = 0;
+    for (i in newjobs){
+    	var q = 'INSERT INTO jobs (job) VALUES ("' + newjobs[i] + '");';
+		db.query(q, function(err, rows){});
+    }
+  });
+  socket.on('gebruiker_nieuw', function(account){
+	var username = getSafeQuery(account.username);
+	var password = getSafeQuery(account.password);
+	
+	var q = 'INSERT INTO accounts (username, password) VALUES ("' + username + '", "' + password + '");';
+	db.query(q, function(err, rows){});
+  });
+  socket.on('login', function(account){
+	var username = getSafeQuery(account.username);
+	var password = getSafeQuery(account.password);
+	
+	var q = 'SELECT password FROM accounts WHERE username = ' + username + ';';
+	db.query(q, function(err, rows){
+		if (rows.length == 0){
+			socket.emit('geen_account', null);
+		}
+		else{
+			
+		}
+	});
+  });
+  socket.on('pform', function(data){
+    console.log("swek ingestuurd");
+  });
 });

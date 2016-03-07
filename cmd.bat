aa2fc9c4ec4f5cb82c7394f71be4e6b0e7e13b6f
Bastiaan van Hoorn <Bastiaan97.bvh@gmail.com>
2016-03-07 19:39:17 +0100

    Migrated to ejs

--- cmd.bat
+++
@@ -1,2 +0,0 @@
-C:\Windows\System32\cmd.exe
-@pause
--- index.js
+++ index.js
@@ -3,43 +3,65 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var mysql = require('mysql');
-
 var db = mysql.createConnection({
-	host: 'localhost',
-	user: 'root',
-	password: 'NEWPASSWORD',
-	database: 'ITC'
+    host: 'localhost',
+    user: 'root',
+    password: 'NEWPASSWORD',
+    database: 'ITC'
 })
 
-db.connect(function(err){
-	if (err)console.log(err)
-})
+//db.connect(function(err){
+//	if (err)console.log(err)
+//})
 
 app.use(express.static(__dirname + '/www'));
-io.on('connection', function(socket){
-  socket.on('zoek', function(msg){
-	var search = "SELECT job FROM jobs WHERE job LIKE '%" + msg + "%';";
-	db.query(search, function(err, rows){
-		if (err) throw err;
-		socket.emit('zoek', rows);
-	});
-  });
-  socket.on('beroep', function(msg){
-    var newjobs = msg.split("+");
-    var i = 0;
-    for (i in newjobs){
-    	var q = 'INSERT INTO jobs (job) VALUES ("' + newjobs[i] + '");';
-	db.query(q, function(err, rows){
+app.set('view engine', 'ejs');
 
-	});
-    }
-  });
-  socket.on('pform', function(data){
-    console.log("swek ingestuurd");
-  });
+
+
+app.get('/', function(req, res) {
+    res.render('../www/index');
+});
+app.get('/index', function(req, res) {
+    res.render('../www/index');
+});
+app.get('/projects', function(req, res) {
+    res.render('../www/projects');
+});
+app.get('/resource', function(req, res) {
+    res.render('../www/resource');
+});
+app.get('/resultaten', function(req, res) {
+    res.render('../www/resultaten');
+});
+app.get('/results', function(req, res) {
+    res.render('../www/results');
 });
 
-http.listen(50002, function(){
-  console.log('listening on *:50002');
+
+io.on('connection', function(socket) {
+    socket.on('zoek', function(msg) {
+        var search = "SELECT job FROM jobs WHERE job LIKE '%" + msg + "%';";
+        db.query(search, function(err, rows) {
+            if (err) throw err;
+            socket.emit('zoek', rows);
+        });
+    });
+    socket.on('beroep', function(msg) {
+        var newjobs = msg.split("+");
+        var i = 0;
+        for (i in newjobs) {
+            var q = 'INSERT INTO jobs (job) VALUES ("' + newjobs[i] + '");';
+            db.query(q, function(err, rows) {
+
+            });
+        }
+    });
+    socket.on('pform', function(data) {
+        console.log("swek ingestuurd");
+    });
 });
 
+http.listen(50002, function() {
+    console.log('listening on *:50002');
+});
